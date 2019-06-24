04f2d616d2351e576c3fc6689cf8a018ff7b7e53
xbl <blxie@toughtworks.com>
2019-06-24 13:00:42 +0800

    refactor(login service):

--- tests/unit/login/service.spec.js
+++ tests/unit/login/service.spec.js
@@ -3,20 +3,13 @@ import MockAdapter from 'axios-mock-adapter';
 import Service from '@/login/service';
 
 describe('Login service', () => {
-  it('Given 用户数据为 xbl,123, When 执行 Service.login() 时，Then 参数为 xbl,123 的用户', (done) => {
+  it('Given 用户数据为 xbl,123, When 执行 Service.login() 时，Then 参数为 xbl,123 的用户', async () => {
     const mock = new MockAdapter(axios);
     const expectedResult = { username: 'xbl', password: '123' };
-    mock.onPost('/users/token').reply((config) => {
-      const user = JSON.parse(config.data);
+    mock.onPost('/users/token').reply(200);
 
-      expect(user).toEqual(user);
-      done();
-
-      return [200, {
-        token: 'xxx-yyy.abc',
-      }];
-    });
-
-    Service.login(expectedResult);
+    await Service.login(expectedResult);
+    expect(mock.history.post.length).toBe(1);
+    expect(mock.history.post[0].data).toBe(JSON.stringify(expectedResult));
   });
 });
