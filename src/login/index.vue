8e45d09eddc6cb96d4bf8e27cda6950f670f0dda
xbl <blxie@toughtworks.com>
2019-06-25 17:56:45 +0800

    style(*): add css

--- src/login/index.vue
+++ src/login/index.vue
@@ -1,7 +1,14 @@
+<style scoped>
+ul { list-style: none; width: 250px; margin: 0 auto; text-align: right; font-size: 14px; }
+ul li { line-height: 30px; display: flex; justify-content: flex-end; margin-bottom: 10px; }
+ul span { display: inline-block; width: 60px; }
+ul input { flex: 1; }
+</style>
+
 <template>
   <ul>
-    <li>用户名：<input type="text" class="username" v-model="user.username"></li>
-    <li>密码：<input type="text" class="password" v-model="user.password"></li>
+    <li><span>用户名：</span><input type="text" class="username" v-model="user.username"></li>
+    <li><span>密码：</span><input type="text" class="password" v-model="user.password"></li>
     <li><button class="submit" @click="onSubmit" :disabled="!validate">提交</button></li>
   </ul>
 </template>
--- src/router.js
+++ src/router.js
@@ -11,8 +11,8 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
