d01e377241d5311aec42dc7aa55b0b1f200b75ec
Chen Chongfa <cfachen@thoughtworks.com>
2021-09-17 11:02:19 +0800

    temp commit
    Signed-off-by: Chen Chongfa <cfachen@thoughtworks.com>

--- src/login/service.js
+++ src/login/service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const login = (user) => axios.post('/users/token', user);
-
+const logout = (user) => axios.post(                    '/users/token/revoke', user);
 export default {
   login,
 };
