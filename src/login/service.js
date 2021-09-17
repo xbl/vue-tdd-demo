d72c28807dfc5d07b4be753e5e3f56b7099f722e
Chen Chongfa <cfachen@thoughtworks.com>
2021-09-17 11:33:28 +0800

    fix(login): remove useless code
    Signed-off-by: Chen Chongfa <cfachen@thoughtworks.com>

--- src/login/service.js
+++ src/login/service.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-const avar = 4;
-avar = 5;
-
 const login = (user) => axios.post(null, user);
-const logout = (user) => axios.post(                    '/users/token/revoke', user);
 export default {
   login,
 };
